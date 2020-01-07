e06af5dedd444d24dca9a50f35e98768ffce2d1e
Corey Hodge <contact@coreyhodge.net>
2020-01-07 04:26:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,6 +10,16 @@
 # puts say_hello(users_name)
 
 
+
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
 def play(songsArray)
   puts "Please enter a song name or number:"
   songChoice = gets.strip
@@ -37,16 +47,6 @@ def play(songsArray)
   end
   
 end
-
-def help
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
-  puts "- list : displays a list of songs you can play"
-  puts "- play : lets you choose a song to play"
-  puts "- exit : exits this program"
-end
-
-
 
 def list 
   
